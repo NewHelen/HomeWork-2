c5c064aab699857d1c8e45a73e42b630d1814650
Olena Zakharina <strin000@gmail.com>
2024-02-11 21:02:29 +0200

    added changes 3

--- src/main/java/MyLinkedList.java
+++ src/main/java/MyLinkedList.java
@@ -1,2 +1,75 @@
-public class MyLinkedList {
+import java.util.Objects;
+
+public class MyLinkedList <T>{
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    public void add(T value) {
+        Node<T> node = new Node<>(value);
+
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            node.previous = tail;
+            tail = node;
+        }
+        size++;
+    }
+
+    public void remove(int index){
+        if (index <= 0 && index > size){
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+
+        Node<T> currentNode = head;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+
+        if (currentNode.previous != null) {
+            currentNode.previous.next = currentNode.next;
+        } else {
+            head = currentNode.next;
+        }
+
+        if (currentNode.next != null) {
+            currentNode.next.previous = currentNode.previous;
+        } else {
+            tail = currentNode.previous;
+        }
+        size--;
+    }
+
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.value;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+        Node<T> previous;
+
+        public Node(T value) {
+            this.value = value;
+        }
+    }
 }
--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,15 +1,35 @@
+
 public class Run {
     public static void main(String[] args) {
 
+        // task 1
         MyArrayList<String> names = new MyArrayList<>();
         names.add("Mark");
         names.add("John");
         names.add("Carter");
+
         System.out.println("Second name is " + names.get(1));
-        names.remove(0);
-        System.out.println("size is " + names.size());
+        names.remove(1);
+        System.out.println("Second name is " + names.get(1));
+
+        System.out.println("Size is " + names.size());
+
         names.clear();
-        System.out.println("size after clear " + names.size());
+        System.out.println("Size after clear " + names.size());
+
+        // task 2
+        MyLinkedList<String> cars = new MyLinkedList<>();
+        cars.add("BMW");
+        cars.add("FORD");
+        cars.add("GMC");
+
+        System.out.println("Get car with index 1 " + cars.get(1));
+        cars.remove(1);
+        System.out.println("Get car with index 1 " + cars.get(1));
+
+        System.out.println("Size is " + cars.getSize());
+        cars.clear();
+        System.out.println("Size after clear " + cars.getSize());
 
 
 
