b7123f9a17febabd93c6c50199659df0fab12418
Olena Zakharina <strin000@gmail.com>
2024-02-12 19:17:13 +0200

    added changes 4

--- src/main/java/MyQueue.java
+++ src/main/java/MyQueue.java
@@ -1,2 +1,53 @@
-public class MyQueue {
+import java.util.Arrays;
+
+public class MyQueue <T>{
+    private T[] data;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public MyQueue (){
+        this.data = (T[]) new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    public MyQueue(int size) {
+        this.data = (T[]) new Object[size];
+        this.size = 0;
+    }
+
+    public void add(T value) {
+        if (size == data.length) {
+            int newCapacity = data.length * 2;
+            data = Arrays.copyOf(data, newCapacity);
+        }
+        data[size++] = value;
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            data[i] = null;
+        }
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public T peek (){
+        if (size == 0) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        return data[0];
+    }
+
+    public T poll(){
+        if (size == 0) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        T firstElement = data[0];
+        System.arraycopy(data, 1, data, 0, --size);
+        return firstElement;
+    }
+
 }
--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Run {
     public static void main(String[] args) {
@@ -7,13 +9,10 @@ public class Run {
         names.add("Mark");
         names.add("John");
         names.add("Carter");
-
         System.out.println("Second name is " + names.get(1));
         names.remove(1);
         System.out.println("Second name is " + names.get(1));
-
         System.out.println("Size is " + names.size());
-
         names.clear();
         System.out.println("Size after clear " + names.size());
 
@@ -22,14 +21,54 @@ public class Run {
         cars.add("BMW");
         cars.add("FORD");
         cars.add("GMC");
-
         System.out.println("Get car with index 1 " + cars.get(1));
         cars.remove(1);
         System.out.println("Get car with index 1 " + cars.get(1));
-
         System.out.println("Size is " + cars.getSize());
         cars.clear();
         System.out.println("Size after clear " + cars.getSize());
+
+        // task 3
+        MyQueue<String> queue = new MyQueue<>();
+        queue.add("User 1");
+        queue.add("User 2");
+        queue.add("User 3");
+
+        System.out.println("Return first element from queue: " + queue.peek());
+        System.out.println("Return first element and remove it: " + queue.poll());
+        System.out.println("Return first element from queue: " + queue.peek());
+        System.out.println("Size is " + queue.size());
+        queue.clear();
+        System.out.println("Size after clear " + queue.size());
+
+        // Task 4
+//        push(Object value) додає елемент в кінець
+//        remove(int index) видаляє елемент за індексом
+//        clear() очищає колекцію
+//        size() повертає розмір колекції
+//        peek() повертає перший елемент стеку
+//        pop() повертає перший елемент стеку та видаляє його з колекції
+
+
+        // task 5
+        Map <String,Integer> map = new HashMap<>();
+        map.put("test",1);
+        map.put("tes",4);
+        map.put("te",7);
+        System.out.println(map.get("test"));
+
+//        Потрібно робити за допомогою однозв'язної Node.
+//
+//        Не може зберігати дві ноди з однаковими ключами.
+//
+//        put(Object key, Object value) додає пару ключ + значення
+//        remove(Object key) видаляє пару за ключем
+//        clear() очищає колекцію
+//        get(Object key) повертає значення (Object value) за ключем
+
+
+
+
 
 
 
